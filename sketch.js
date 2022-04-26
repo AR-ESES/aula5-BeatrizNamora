2b99ec45f939a1a11444ef706ff477d9e8a2c44d
BeatrizNamora <102222010+BeatrizNamora@users.noreply.github.com>
2022-04-26 11:08:37 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,10 +8,31 @@ function setup() {
 
 function draw() {
 
+	background(0)
+
+
+if(mouseY >= height/2) {
+	noFill();
+}
+
+else{
+
+	fill(12,108,57); //verde
+}
+
+
+if(mouseX <= width/2) {
+	fill(19,52,146); //azul
+}
+
+if(mouseY < height/2 && mouseX <= width/2) {
+	fill(221,56,56); //vermelho
+
+}
 	stroke(230);
 	strokeWeight(2);
-	noFill();
-	circle(width / 2, height / 2, height / 4);
+	
+	circle(width / 2, height / 2, height / 4); // circulo sempre ao centro do encrã
 
 
 }
